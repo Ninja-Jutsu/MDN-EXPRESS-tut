b3d9e19df4d326fde2eff0f78e0ed9ad7048075d
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-04-27 18:27:11 +0800

    Complete bookinstance routes

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -93,7 +93,7 @@ exports.bookinstance_create_post = [
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = async (req, res, next) => {
   // Get details of genre
-  const bookinstance = BookInstance.findById(req.params.id).exec()
+  const bookinstance = await BookInstance.findById(req.params.id).exec()
 
   if (bookinstance === null) {
     // No results.
@@ -108,23 +108,87 @@ exports.bookinstance_delete_get = async (req, res, next) => {
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = async (req, res, next) => {
-  const bookinstance = BookInstance.findById(req.params.id).exec()
+  const bookinstance = await BookInstance.findById(req.params.id).exec()
 
   if (bookinstance === null) {
     // No results.
     res.redirect('/catalog/bookinstances')
   }
 
-  await BookInstance.findByIdAndDelete(req.params.id)
+  await BookInstance.findByIdAndDelete(req.body.bookinstanceid)
   res.redirect('/catalog/bookinstances')
 }
 
 // Display BookInstance update form on GET.
 exports.bookinstance_update_get = async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance update GET')
+  // Get book, all books for form (in parallel)
+  const [bookInstance, allBooks] = await Promise.all([
+    BookInstance.findById(req.params.id).populate('book').exec(),
+    Book.find(),
+  ])
+
+  if (bookInstance === null) {
+    // No results.
+    const err = new Error('Book copy not found')
+    err.status = 404
+    return next(err)
+  }
+
+  res.render('bookinstance_form', {
+    title: 'Update BookInstance',
+    book_list: allBooks,
+    selected_book: bookInstance.book._id,
+    bookinstance: bookInstance,
+  })
 }
 
 // Handle bookinstance update on POST.
-exports.bookinstance_update_post = async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance update POST')
-}
+exports.bookinstance_update_post = [
+  // Validate and sanitize fields.
+  body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
+  body('imprint', 'Imprint must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('status').escape(),
+  body('due_back', 'Invalid date')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req)
+
+    // Create a BookInstance object with escaped/trimmed data and current id.
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+      _id: req.params.id,
+    })
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Render the form again, passing sanitized values and errors.
+
+      const allBooks = await Book.find({}, 'title').exec()
+
+      res.render('bookinstance_form', {
+        title: 'Update BookInstance',
+        book_list: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookInstance,
+      })
+      return
+    } else {
+      // Data from form is valid.
+      await BookInstance.findByIdAndUpdate(req.params.id, bookInstance, {})
+      // Redirect to detail page.
+      res.redirect(bookInstance.url)
+    }
+  },
+]
