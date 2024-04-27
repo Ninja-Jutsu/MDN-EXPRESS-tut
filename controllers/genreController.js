deb766f5fa02cff1fbc2280b896055e613367159
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-04-27 12:29:18 +0800

    Create Genre creator view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,5 +1,6 @@
 const Genre = require('../models/genre')
 const Book = require('../models/book')
+const { body, validationResult } = require('express-validator')
 // const asyncHandler = require('express-async-errors')
 
 // Display list of all Genre.
@@ -33,14 +34,50 @@ exports.genre_detail = async (req, res, next) => {
 }
 
 // Display Genre create form on GET.
-exports.genre_create_get = async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre create GET')
+exports.genre_create_get = (req, res, next) => {
+  res.render('genre_form', { title: 'Create Genre' })
 }
 
 // Handle Genre create on POST.
-exports.genre_create_post = async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre create POST')
-}
+exports.genre_create_post = [
+  // Validate and sanitize the name field.
+  body('name', 'Genre name must contain at least 3 characters')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req)
+
+    // Create a genre object with escaped and trimmed data.
+    const genre = new Genre({ name: req.body.name })
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('genre_form', {
+        title: 'Create Genre',
+        genre: genre,
+        errors: errors.array(),
+      })
+      return
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      const genreExists = await Genre.findOne({ name: req.body.name })
+        .collation({ locale: 'en', strength: 2 }) // for case insensitivity (check saved doc)
+        .exec()
+      if (genreExists) {
+        // Genre exists, redirect to its detail page.
+        res.redirect(genreExists.url)
+      } else {
+        await genre.save()
+        // New genre saved. Redirect to genre detail page.
+        res.redirect(genre.url)
+      }
+    }
+  },
+]
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = async (req, res, next) => {
