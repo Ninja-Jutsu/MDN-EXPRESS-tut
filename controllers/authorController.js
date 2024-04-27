ec7cc1acc1d743648dd59c5303a6d7c886620e60
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-04-27 12:51:19 +0800

    Create Add new author view

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,5 @@
 require('express-async-errors')
-
+const { body, validationResult } = require('express-validator')
 const Author = require('../models/author')
 const Book = require('../models/book')
 
@@ -16,9 +16,9 @@ exports.author_list = async (req, res, next) => {
 exports.author_detail = async (req, res, next) => {
   const [author, allBooksByAuthor] = await Promise.all([
     Author.findById(req.params.id).sort({ family_name: 1 }).exec(),
-    Book.find({author : req.params.id}, "title summary").exec()
+    Book.find({ author: req.params.id }, 'title summary').exec(),
   ])
-  if(author === null){
+  if (author === null) {
     const err = new Error('no such book')
     res.status(404)
     return next(err)
@@ -26,19 +26,72 @@ exports.author_detail = async (req, res, next) => {
   res.render('author_detail', {
     title: 'Author Detail',
     author,
-    author_books: allBooksByAuthor
+    author_books: allBooksByAuthor,
   })
 }
 
 // Display Author create form on GET.
-exports.author_create_get = async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Author create GET')
+exports.author_create_get = (req, res, next) => {
+  res.render('author_form', { title: 'Create Author' })
 }
 
 // Handle Author create on POST.
-exports.author_create_post = async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Author create POST')
-}
+exports.author_create_post = [
+  // Validate and sanitize fields.
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+  body('family_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Family name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters.'),
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(), // change it to a valid JS date format
+  body('date_of_death', 'Invalid date of death')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req)
+
+    // Create Author object with escaped and trimmed data
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    })
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render('author_form', {
+        title: 'Create Author',
+        author: author,
+        errors: errors.array(),
+      })
+      return
+    } else {
+      // Data from form is valid.
+
+      // Save author.
+      await author.save()
+      // Redirect to new author record.
+      res.redirect(author.url)
+    }
+  },
+]
 
 // Display Author delete form on GET.
 exports.author_delete_get = async (req, res, next) => {
